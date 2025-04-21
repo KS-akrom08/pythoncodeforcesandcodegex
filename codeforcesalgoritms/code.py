3ac61ba4bce56554f450abde50abbf1c720d85de
KS-akrom08 <akrombekruziev080808@gmail.com>
2025-04-21 18:48:57 +0500

    Add files via upload

---
+++ codeforcesalgoritms/code.py
@@ -0,0 +1,45 @@
+# test 1
+# a = input('Enter number of watermelon weight ')
+# if int(a) %2 == 0:
+#     print('yes')
+# else:
+#     print('no')
+# test 2    
+# a = input('enter a word ')
+# if len(a)>10 :
+#     print(f"{a[0]}{len(a)-2}{a[-1]}")
+# else:
+#     print(a)    
+# test 3
+# number_of_questions = 3
+# first = [1,1,0]
+# second = [1,1,1]
+# third = [1,0,0]
+# firstsum = sum(first)
+# secondsum = sum(second)
+# thirdsum = sum(third)
+# a = [thirdsum,secondsum,firstsum]
+# b = 0
+# for i in a:
+#     if i >= 2:
+#        b += 1 
+#     else:
+#        0
+# print(b)
+# test 4           
+# a = 0
+# b = input('enter operation ++x or --x:').split()
+# for i in b:
+#     if i == '++x':
+#        a +=1
+#     elif i == '--x':
+#        a -= 1
+#     else:
+#        print('invalid operation')
+# print(a)
+# test 5
+# a = input('enter lenght of palce and lenght: ')
+# b = int(input('enter lenght of squared plits: '))
+# a = a.split()
+# print(int(a[0])int(a[1])/b**2)
+# test 6
