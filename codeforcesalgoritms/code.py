1ab019277a81cced9f77b41b3af1196366bccc52
KS-akrom08 <akrombekruziev080808@gmail.com>
2025-11-05 20:54:10 +0500

    Add files via upload

--- codeforcesalgoritms/code.py
+++ codeforcesalgoritms/code.py
@@ -477,30 +477,37 @@
 # test 46
 # a = int(input('enter number: '))
 # print(5**a)
-a = int(input('enter number of matchs: '))
-match_points = []
+# a = int(input('enter number of matchs: '))
+# match_points = []
 
-score = 0
-for _ in range(a):
-    b = int(input('enter score: '))
-    match_points.append(b)
-min = match_points[0]
-max = match_points[0]
-for i in match_points[1:]:
-    if i > min and i < max:
-        score += 0
-    elif i>max:
-        score += 1
-        max = i
-    elif i< min:
-        score += 1
-        min = i
-    else:
-        score += 0
+# score = 0
+# for _ in range(a):
+#     b = int(input('enter score: '))
+#     match_points.append(b)
+# min = match_points[0]
+# max = match_points[0]
+# for i in match_points[1:]:
+#     if i > min and i < max:
+#         score += 0
+#     elif i>max:
+#         score += 1
+#         max = i
+#     elif i< min:
+#         score += 1
+#         min = i
+#     else:
+#         score += 0
     
-print(score)        
-    
-    
+# print(score)        
+a = ['H', 'Q', '9', '+']
+b = input('enter a command: ')
+c = 'NO'
+for i in b:
+    for j in a:
+       if i == j:
+           c = 'YES'
+print(c)       
+            
     
 
    
